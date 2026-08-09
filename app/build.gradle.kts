89ed0eb6d48c962797a23e62514c04a6a8d21d2c
An-K4 <ana5k44nknd@gmail.com>
2026-08-09 22:39:18 +0700

    add testing libs to app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,10 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.kotlinx.serialization.json)
 
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
     // Hilt Entry Point
     implementation(libs.hilt.android)
     ksp(libs.hilt.compiler)
